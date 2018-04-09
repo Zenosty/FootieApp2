5bf352362c374339b414da36f73b9fe9afa3d210
Stephen <stephen.hickey007@gmail.com>
2018-04-09 21:10:47 +0100

    Q

--- ConsoleApplication1/ConsoleApplication1/CFootballTeam.cpp
+++ ConsoleApplication1/ConsoleApplication1/CFootballTeam.cpp
@@ -23,3 +23,78 @@ CFootballTeam::CFootballTeam(string name)
 	, m_Points(0)
 {
 }
+
+CFootballTeam::CFootballTeam(string name, int gamesPlayed, int goalsFor, int goalsAgainst, int points)
+{
+	m_Name = name;
+	m_GamesPlayed = gamesPlayed;
+	m_GoalsFor = goalsFor;
+	m_GoalsAgainst = goalsAgainst;
+	m_Points = points;
+}
+
+string CFootballTeam::GetName(void)
+{
+	return m_Name;
+}
+
+int CFootballTeam::GetGamesPlayed(void)
+{
+	return m_GamesPlayed;
+}
+
+int CFootballTeam::GetGoalsFor(void)
+{
+	return m_GoalsFor;
+}
+
+int CFootballTeam::GetGoalsAgainst(void)
+{
+	return m_GoalsAgainst;
+}
+
+int CFootballTeam::GetPoints(void)
+{
+	return m_Points;
+}
+
+bool CFootballTeam::HasName(string SearchName)
+{
+	if (m_Name == SearchName)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void CFootballTeam::Display(void)
+{
+	cout << setw(10) << m_Name 
+		<< setw(10) << m_GamesPlayed
+		<< setw(10) << m_GoalsFor
+		<< setw(10) << m_GoalsAgainst 
+		<< setw(10) << m_Points << endl;
+
+}
+
+void CFootballTeam::UpdateOnResult(int goalsFor, int goalsAgainst)
+{
+	m_GoalsFor += goalsFor;
+	m_GoalsAgainst += goalsAgainst;
+
+	if (goalsFor > goalsAgainst)
+	{
+		m_Points += 3;
+	}
+	else if (goalsFor == goalsAgainst)
+	{
+		m_Points += 1;
+	}
+}
+
+void CFootballTeam::DeductPoints(int num)
+{
+	m_Points -= num;
+}
